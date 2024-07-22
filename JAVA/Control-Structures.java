0fd88f180523f6b7a05ededfd3d203945a575e71
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2024-07-22 23:35:40 +0530

    Create Control-Structures.java

---
+++ JAVA/Control-Structures.java
@@ -0,0 +1,23 @@
+Control Structures:
+
+  If-Else:
+
+if (condition) {
+  // code block
+} else {
+  // code block
+}
+
+Switch:
+
+switch (expression) {
+  case value1:
+    // code block
+break;
+  case value2:
+    // code block
+break;
+  default: 
+// code block
+}
+
