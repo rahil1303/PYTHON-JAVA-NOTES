32e64a6a49c42e49bebbfea233881d3b272f6e39
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2024-09-10 08:44:08 +0200

    Create Python_Problem_1.py

---
+++ Python_Problem_1.py
@@ -0,0 +1,13 @@
+def two_sum[nums, target]:
+  seen = {}
+
+  for i, num in enumerate(nums):
+    complement = target - num
+
+    if complement in seen:
+      return [seen[complement],i]
+    else:
+      seen[num] = i
+
+return[]
+  
